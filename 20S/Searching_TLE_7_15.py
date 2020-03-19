63c2b7090950d12877872daf4169fab0e569a4da
Lawrenc3X <57817583+Lawrenc3X@users.noreply.github.com>
2020-03-19 14:40:49 -0700

    Update Searching_TLE_7_15.py

--- 20S/Searching_TLE_7_15.py
+++ 20S/Searching_TLE_7_15.py
@@ -23,7 +23,20 @@ previous = []
 #     if Occurences(view) == signature:
 #         if view not in previous:
 #             perm += 1
-#                previous.append(view)
+#             previous.append(view)
 rolling_view = Occurences(haystack[0: needle_length])
+
+if rolling_view == signature:
+    perm += 1
+    previous.append(haystack[:needle_length])
+
+for i in range(1, len(haystack) - needle_length + 1):
+    rolling_view[ord(haystack[i - 1]) - 97] -= 1
+    rolling_view[ord(haystack[i + needle_length - 1]) - 97] += 1
+    permutation = haystack[i:i + needle_length]
+
+    if rolling_view == signature and permutation not in previous:
+        perm += 1
+        previous.append(permutation)
 
 print(perm)
