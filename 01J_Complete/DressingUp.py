d297c7f70c1e86e5cb01668863802b0711a0e5a5
Lawrenc3X <57817583+Lawrenc3X@users.noreply.github.com>
2020-06-10 14:38:48 -0700

    removed everything

--- 01J_Complete/DressingUp.py
+++
@@ -1,6 +0,0 @@
-height = int(input())
-for i in range(-(2 * height - 2), 2 * height - 1, 4):
-    space = abs(i)
-    stars = ((2 * height - space)//2)
-    line  = "*" * stars + " " * space + "*" * stars
-    print(line)
